0269ae5e2646a9821d18e2eed4dc77c11f5c6af3
Anderson Miles <andersonmiles@192.168.124.21>
2024-08-31 21:44:46 +0800

    added selection highlighting,removal of projects

--- src/components/ProjectCmp.jsx
+++ src/components/ProjectCmp.jsx
@@ -1,12 +1,20 @@
 import RenameProjectCmp from "./RenameProjectCmp.jsx";
 import DeleteProjectCmp from "./DeleteProjectCmp.jsx";
 import '../App.css'
-export default function ProjectCmp(){
+import {useState} from "react";
+export default function ProjectCmp({project,select_PFn, removeProjects_PFn,isSelected}){
+    const [name,setName]=useState(project.name);
+    const id = project.id;
+    function handleOnClickEvent(){
+        console.log(`Project ${project.id} isSelected:`, isSelected);
+        select_PFn(); // Call the select function to update the selected project in parent
+    }
     return(
-        <div className="ProjectCmp">
-            <label>ProjectName:</label>
+        <div className={`ProjectCmp ${isSelected ? 'selected' : ''}`}
+             onClick={handleOnClickEvent}>
+            <label>{name}:</label>
             <RenameProjectCmp />
-            <DeleteProjectCmp/>
+            <DeleteProjectCmp removeProjects_PFn={removeProjects_PFn}/>
         </div>
     )
 }
